3482b90e044f7ee662ed7563005b5ba69c6379bc
YZan <2961153574@qq.com>
2026-03-31 13:22:42 +0800

    初始化

---
+++ src/main/java/com/yzan/yzan_multi_agent/YZanMultiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.yzan.yzan_multi_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YZanMultiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YZanMultiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yzan/yzan_multi_agent/YZanMultiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzan.yzan_multi_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YZanMultiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
